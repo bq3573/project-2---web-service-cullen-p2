34ba49e0593dd8a99e30fe2943a1a3e0dae4af45
Cullen Waller <wallercl@dukes.jmu.edu>
2021-10-29 03:08:52 +0000

    Service code

--- project2-service/index.js
+++ project2-service/index.js
@@ -21,19 +21,35 @@ service.use(express.json());
 service.get('/posts/:id', (request, response) => {
   const id = parseInt(request.params.id);
 
-  const query = 'SELECT * FROM posts WHERE id = ? AND is_visible != 0';
-  connection.query(query, id, (error, rows) => {
-    if (error) {
+  doesExist(id, function(result) {
+    if (result > 0) {
+      const query = 'SELECT * FROM posts WHERE id = ? AND is_visible != 0';
+      connection.query(query, id, (error, rows) => {
+        if (error) {
+          response.status(500);
+          response.json({
+            ok: false,
+            results: error.message
+          });
+        } else {
+          const post = rows.map(rowToMem);
+          response.json({
+            ok: true,
+            results: rows.map(rowToMem)
+          });
+        }
+      });
+    } else if (result == 0) {
+      response.status(404);
+      response.json({
+        ok: false,
+        results: "Post does not Exist"
+      });
+    } else {
       response.status(500);
       response.json({
         ok: false,
-        results: error.message
-      });
-    } else {
-      const post = rows.map(rowToMem);
-      response.json({
-        ok: true,
-        results: rows.map(rowToMem)
+        results: "Database Error"
       });
     }
   });
@@ -87,10 +103,160 @@ service.post('/posts', (request, response) => {
   }
 });
 
+service.post('/comment/:id', (request, response) => {
+  if (request.body.hasOwnProperty('content')) {
+    const params = [
+      parseInt(request.params.id),
+      request.body.content
+    ];
+
+    doesExist(params[0], function(result) {
+      if (result > 0) {
+        const query = 'INSERT INTO posts(parent_id, content) VALUES(?, ?)';
+        connection.query(query, params, (error, result) => {
+          if (error) {
+            response.status(500);
+            response.json({
+              ok: false,
+              results: error.message
+            });
+          } else {
+	    const insertId = result.insertId
+            const query2 = 'UPDATE posts SET comments = comments + 1 WHERE id = ?';
+            connection.query(query2, params[0], (error, result) => {
+              if (error) {
+                response.status(500);
+                response.json({
+                  ok: false,
+                  results: error.message
+                });
+              } else {
+                response.json({
+                  ok: true,
+                  results: insertId
+                });
+              }
+            });
+          }
+        });
+      } else if (result == 0) {
+        response.status(404);
+        response.json({
+          ok: false,
+          results: "Post does not exist"
+        });
+      } else {
+	      response.status(500);
+	      response.json({
+	        ok: false,
+	        results: "Database error"
+      	});
+      }
+    });
+  } else {
+    response.status(400);
+    response.json({
+      ok: false,
+      results: 'Bad request'
+    });
+  }
+});
+
+service.patch('/like/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+
+  doesExist(id, function(result) {
+    if (result > 0) {
+      const query = 'UPDATE posts SET likes = likes + 1 WHERE id = ?';
+      connection.query(query, id, (error, result) => {
+        if (error) {
+          response.status(500);
+          response.json({
+            ok: false,
+            results: error.message
+          });
+        } else {
+          response.json({
+            ok: true
+          });
+        }
+      });
+    } else if (result == 0) {
+      response.status(404);
+      response.json({
+        ok: false,
+        results: "Post does not exist"
+      });
+    } else {
+      response.status(400);
+      response.json({
+        ok: false,
+        results: "Database error"
+       });
+    }
+  });
+});
+
+service.delete('/posts/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+
+  doesExist(id, function(result) {
+    if (result > 0) {
+      const query = "UPDATE posts SET is_visible = 0 WHERE id = ?";
+
+      connection.query(query, id, (error, result) => {
+        if (error) {
+          response.status(500);
+          response.json({
+            ok: false,
+            results: error.message
+          });
+        } else {
+          response.json({
+            ok: true
+          });
+        }
+      });
+    } else if (result == 0) {
+      response.status(404);
+      response.json({
+        ok: false,
+        results: "Post does not exist"
+      });
+    } else {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: "Database error"
+      });
+    }
+  });
+});
+
 const port = 5001;
 service.listen(port, () => {
   console.log(`We're live on port ${port}!`);
 });
+
+/** 
+ * Checks if post with given id exists.
+ * 
+*/
+function doesExist(id, callback) {
+  const query = 'SELECT * FROM posts WHERE id = ?';
+  connection.query(query, id, (error, result) => {
+    if (error) {
+      return callback(-1);
+    } else {
+      if (result && result.length) {
+	console.log(result)
+     	  return callback(result[0].id);
+      } else {
+	      return callback(0);
+      }
+    }
+  });
+}
 
 function rowToMem(row) {
   return {
